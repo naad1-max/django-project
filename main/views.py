756420c3a2f1bf436cb473c8d6a605081b05096f
Naad Bhave <bhavenaad@gmail.com>
2026-03-27 15:51:55 +0530

    Added template rendering

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index_view(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "main/index.html")
